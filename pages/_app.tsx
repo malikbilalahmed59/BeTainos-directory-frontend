13c8d51e9b016acc4473df4dba95d08002155a3a
Qamar Ali <bisolqamar01@gmail.com>
2024-10-19 09:00:38 -0700

    header done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import "@/styles/globals.css";
+
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle.min";
+import "@/styles/landing.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
---
+++ pages/components/landing-page/Header.tsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import Image from "next/image";
+import Link from 'next/link';
+import Logo from "/public/images/logo.png"
+
+export const Header = () => {
+  return (
+    <div className='header-main-con w-100 float-start'>
+        <div className='top-bar-con'>
+            <div className='container'>
+                <div className='top-bar-box'>
+                    <div className='top-bar-text'>
+                        <span className='d-block'>The 100% Afro-Caribbean director</span>
+                    </div>
+                    <div className='top-bar-info'>
+                        <div className='input-block'>
+                        <input type='text' placeholder='Search'/>
+                        </div>
+                        <div className='social-icon'>
+                        <ul className='list-unstyled mb-0'>
+                            <li>
+                                <Link href="/"><i className="fa-brands fa-facebook-f"></i></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><i className="fa-brands fa-instagram"></i></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><i className="fa-brands fa-x-twitter"></i></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><i className="fa-brands fa-youtube"></i></Link>
+                            </li>
+                            <li>
+                                <Link href="/"><i className="fa-brands fa-tiktok"></i></Link>
+                            </li>
+                        </ul>
+                        {/* social-icon */}
+                        </div>
+                    </div>
+                    {/* top-bar-box */}
+                </div>
+                {/* container */}
+            </div>
+        {/* top-bar */}
+        </div>
+        <header className=' header-con'>
+        <div className='container'>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <Link href="/" className="navbar-brand">
+                    <figure className='mb-0'>
+                    <Image width={216} height={63} src={Logo} alt="logo" />
+                    </figure>
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto mb-lg-0">
+                        <li className="nav-item">
+                            <Link href="/" className="nav-link" aria-current="page">
+                            Directory</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/blog" className="nav-link">
+                            Blog pro</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/offers" className="nav-link">
+                            Offers</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/contact" className="nav-link">
+                            Contact</Link>
+                        </li>
+                    </ul>
+                    <div className='cart-btn'>
+                        <Link href='#' className="nav-link">
+                        Add</Link>
+                        <Link href='#' className="nav-link">
+                        Login</Link>
+                    </div>
+                    </div>
+                </div>
+            </nav>
+            {/* container */}
+        </div>
+        {/* header-con */}
+        </header>
+    {/* header-main-con */}
+    </div>
+  )
+}
