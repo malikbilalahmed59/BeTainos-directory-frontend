a4d77aa78349bdbbf3e4ee066b3283270b478f01
Qamar Ali <bisolqamar01@gmail.com>
2024-10-19 09:20:26 -0700

    advertise-banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
 import { Header } from "./components/landing-page/Header";
+import { AdvertiseBanner } from "./components/landing-page/AdvertiseBanner";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,6 +27,7 @@ export default function Home() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
       </Head>
       <Header/>
+      <AdvertiseBanner/>
     </>
   );
 }
