230f75b4457586f85391aef471c49db0d376c521
Touseef Ahmed <touseefahmed1214@gmail.com>
2024-10-25 02:50:10 +0500

    slider add

--- pages/components/landing-page/AdvertiseBanner.tsx
+++ pages/components/landing-page/AdvertiseBanner.tsx
@@ -1,5 +1,7 @@
 import { s3BucketStrapiUrl } from '@/app/helper/helper';
 import { ILandingPage } from '@/app/types/landingpage';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 import Image from 'next/image';
 
 interface Props {
@@ -11,11 +13,38 @@ const AdvertiseBanner = ({ pageData }: Props) => {
       {
         pageData?.Advertisement && pageData.Advertisement.Banner && <div className='advertise-banner w-100 float-start'>
           <div className='container'>
-            <figure className='mb-0'>
-              <Image priority width={1000}
-                height={304}
-                src={s3BucketStrapiUrl(pageData.Advertisement.Banner)} alt={pageData.Advertisement.Banner.alternativeText || ""} />
-            </figure>
+          <Swiper
+  spaceBetween={50}
+  slidesPerView={1}
+  loop={true}
+  autoplay={{ 
+    delay: 3000, // Slide transition delay (in ms)
+    disableOnInteraction: false, // Enable/Disable autoplay on user interaction
+  }}
+>
+  <SwiperSlide>
+    <figure className='mb-0'>
+      <Image 
+        priority 
+        width={1000} 
+        height={304} 
+        src={s3BucketStrapiUrl(pageData.Advertisement.Banner)} 
+        alt={pageData.Advertisement.Banner.alternativeText || ""} 
+      />
+    </figure>
+  </SwiperSlide>
+  <SwiperSlide>
+    <figure className='mb-0'>
+      <Image 
+        priority 
+        width={1000} 
+        height={304} 
+        src={s3BucketStrapiUrl(pageData.Advertisement.Banner)} 
+        alt={pageData.Advertisement.Banner.alternativeText || ""} 
+      />
+    </figure>
+  </SwiperSlide>
+</Swiper>
             {/* container */}
           </div>
         </div>
