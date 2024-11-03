af2226dacc600b9ee83b474c3ca952d698910974
adnan425 <166127663+adnan425@users.noreply.github.com>
2024-11-03 16:08:47 +0400

    add company form

---
+++ pages/dashboard/AddCompany.tsx
@@ -0,0 +1,260 @@
+import { useCategories } from '@/app/hooks/useAPIs';
+import InfoOutlineIcon from '@rsuite/icons/InfoOutline';
+import PlusIcon from '@rsuite/icons/Plus';
+import TrashIcon from '@rsuite/icons/Trash';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import {
+    Button,
+    ButtonToolbar,
+    Divider,
+    Form,
+    IconButton,
+    Input,
+    Message,
+    Popover,
+    SelectPicker,
+    Stack,
+    Text,
+    Whisper
+} from 'rsuite';
+
+const Textarea = React.forwardRef<HTMLInputElement, any>((props, ref: any) => (
+    <Input {...props} as="textarea" ref={ref} />
+));
+Textarea.displayName = "TextArea";
+
+interface IState {
+    logo: string;
+    name: string;
+    postalAddress: string;
+    phone: string;
+    email: string;
+    website: string;
+    socials: {
+        icon: string;
+        link: string;
+        name: string;
+    }[];
+    coFounderName: string;
+    founderName: string;
+    categoriesList: string[];
+    description: string;
+    fieldOfExpertise: string;
+}
+
+const initialState: IState = {
+    logo: '',
+    name: '',
+    postalAddress: '',
+    phone: '',
+    email: '',
+    website: '',
+    socials: [],
+    coFounderName: '',
+    founderName: '',
+    categoriesList: [],
+    description: '',
+    fieldOfExpertise: '',
+};
+
+const AddCompany = () => {
+    const [data, setData] = useState<IState>(initialState);
+    const [formError, setFormError] = useState<{ [key: string]: string }>({});
+    const { data: catList, isLoading: catLoading } = useCategories()
+    const triggerRef = React.useRef<any>();
+    const handleChange = (value: Partial<IState>) => {
+        setData({ ...data, ...value });
+    };
+
+    const validate = () => {
+        const errors: { [key: string]: string } = {};
+
+        if (!data.name) {
+            errors.name = 'Name is required';
+        }
+        if (!data.postalAddress) {
+            errors.postalAddress = 'Postal Address is required';
+        }
+        if (!data.description) {
+            errors.description = 'Description is required';
+        }
+        if (!data.phone) {
+            errors.phone = 'Phone is required';
+        }
+        if (!data.founderName) {
+            errors.phone = 'Founder Name is required';
+        }
+        if (!data.email) {
+            errors.email = 'Email is required';
+        } else if (!/\S+@\S+\.\S+/.test(data.email)) {
+            errors.email = 'Invalid email format';
+        }
+        // if (!data.website) {
+        //     errors.website = 'Website is required';
+        // } else if (!/^https?:\/\/\S+\.\S+$/.test(data.website)) {
+        //     errors.website = 'Invalid website URL';
+        // }
+
+        setFormError(errors);
+        return Object.keys(errors).length === 0;
+    };
+
+    const handleSubmit = () => {
+        if (!validate()) {
+            console.log('Validation failed:', JSON.stringify(formError));
+            return;
+        }
+        console.log('Form submitted:', data);
+    };
+
+    const handleAddSocial = () => {
+        setData(prevData => ({
+            ...prevData,
+            socials: [...prevData.socials, { icon: '', link: '', name: '' }]
+        }));
+    };
+
+    const handleDeleteSocial = (index: number) => {
+        setData(prevData => ({
+            ...prevData,
+            socials: prevData.socials.filter((_, i) => i !== index)
+        }));
+    };
+
+    const handleSocialChange = (index: number, field: keyof IState["socials"][0], value: string) => {
+        const updatedSocials = [...data.socials];
+        updatedSocials[index][field] = value;
+        setData({ ...data, socials: updatedSocials });
+    };
+    const speaker = (<Popover title="Social Icons">
+        <span>
+            <Link href={'https://lucide.dev/icons/'} target='_blank'>Click here</Link>&nbsp; to download the icons.
+        </span>
+
+    </Popover>)
+    const catsList = catList?.map(item => ({ label: item.Name, value: item.Name }))
+
+    return (
+        <div className='profile-box generictab-box'>
+            <Message>
+                Please fill out the form below to add company details. Fields marked as required must be completed. You can add multiple social links by clicking &quot;Add Social&quot; and remove any entry with the delete icon.
+            </Message>
+            <Divider />
+            <Form className="basic-form" layout="horizontal">
+                <Form.Group controlId="name">
+                    <Form.ControlLabel>Name*</Form.ControlLabel>
+                    <Form.Control name="name" value={data.name} onChange={(value) => handleChange({ name: value })} />
+                    {formError.name && <Form.HelpText style={{ color: 'red' }}>{formError.name}</Form.HelpText>}
+                </Form.Group>
+
+                <Form.Group controlId="postalAddress">
+                    <Form.ControlLabel>Postal Address*</Form.ControlLabel>
+                    <Form.Control
+                        name="postalAddress"
+                        accepter={Textarea}
+                        value={data.postalAddress}
+                        onChange={(value) => handleChange({ postalAddress: value })}
+                    />
+                    {formError.postalAddress && <Form.HelpText style={{ color: 'red' }}>{formError.postalAddress}</Form.HelpText>}
+                </Form.Group>
+
+                <Form.Group controlId="phone">
+                    <Form.ControlLabel>Phone*</Form.ControlLabel>
+                    <Form.Control name="phone" value={data.phone} onChange={(value) => handleChange({ phone: value })} />
+                    {formError.phone && <Form.HelpText style={{ color: 'red' }}>{formError.phone}</Form.HelpText>}
+                </Form.Group>
+
+                <Form.Group controlId="email">
+                    <Form.ControlLabel>Email*</Form.ControlLabel>
+                    <Form.Control name="email" value={data.email} onChange={(value) => handleChange({ email: value })} />
+                    {formError.email && <Form.HelpText style={{ color: 'red' }}>{formError.email}</Form.HelpText>}
+                </Form.Group>
+
+                <Form.Group controlId="website">
+                    <Form.ControlLabel>Website</Form.ControlLabel>
+                    <Form.Control name="website" value={data.website} onChange={(value) => handleChange({ website: value })} />
+                    {formError.website && <Form.HelpText style={{ color: 'red' }}>{formError.website}</Form.HelpText>}
+                </Form.Group>
+                <Form.Group controlId="founderName">
+                    <Form.ControlLabel>Founder Name*</Form.ControlLabel>
+                    <Form.Control name="founderName" value={data.founderName} onChange={(value) => handleChange({ founderName: value })} />
+                    {formError.founderName && <Form.HelpText style={{ color: 'red' }}>{formError.founderName}</Form.HelpText>}
+                </Form.Group>
+                <Form.Group controlId="selectPicker">
+                    <Form.ControlLabel>Category</Form.ControlLabel>
+                    <Form.Control name="selectPicker" loading={catLoading} accepter={SelectPicker} data={catsList || []} />
+                </Form.Group>
+                <Form.Group controlId="uploader">
+                    <Form.ControlLabel>Logo</Form.ControlLabel>
+                    {/* <Uploader action="#" onUpload={(file) => handleChange({ logo: file.blobFile as string })} /> */}
+                </Form.Group>
+
+                <Form.Group controlId="description">
+                    <Form.ControlLabel>Description*</Form.ControlLabel>
+                    <Form.Control name="description" accepter={Textarea} rows={3} value={data.description} onChange={(value) => handleChange({ description: value })} />
+                    {formError.description && <Form.HelpText style={{ color: 'red' }}>{formError.description}</Form.HelpText>}
+                </Form.Group>
+
+
+                <Form.Group controlId="socials">
+                    <Stack justifyContent='flex-start' alignItems='center' spacing={8}>
+                        <Text weight='bold'>Socials</Text>
+                        <IconButton icon={<PlusIcon />} appearance="primary" onClick={handleAddSocial} />
+                    </Stack>
+                    {data.socials.map((social, index) => (
+                        <div key={index} style={{ marginBottom: '10px' }}>
+                            <Form.Group controlId={`socials-name-${index}`}>
+                                <Form.ControlLabel>Name</Form.ControlLabel>
+                                <Form.Control
+                                    name={`socials-${index}-name`}
+                                    value={social.name}
+                                    onChange={(value) => handleSocialChange(index, 'name', value)}
+                                />
+                            </Form.Group>
+                            <Form.Group controlId={`socials-icon-${index}`}>
+                                <Form.HelpText style={{ position: 'relative' }}>
+                                    <Form.ControlLabel>
+                                        Icon
+                                        <Whisper ref={triggerRef} placement="top" trigger="hover" controlId="control-id-hover" speaker={speaker}>
+                                            <IconButton
+                                                icon={<InfoOutlineIcon />}
+                                                style={{
+                                                    position: 'absolute',
+                                                    top: '-6px',
+                                                    left: '12.2%',
+                                                    zIndex: 99
+                                                }}
+                                                appearance="default"
+                                                size="xs"
+                                            />
+                                        </Whisper>
+                                    </Form.ControlLabel>
+                                </Form.HelpText>
+                                {/* <Uploader action="#" onUpload={(file) => handleSocialChange(index, 'icon', file.blobFile as string)} /> */}
+                            </Form.Group>
+                            <Form.Group controlId={`socials-link-${index}`}>
+                                <Form.ControlLabel>Link</Form.ControlLabel>
+                                <Form.Control
+                                    name={`socials-${index}-link`}
+                                    value={social.link}
+                                    onChange={(value) => handleSocialChange(index, 'link', value)}
+                                />
+                            </Form.Group>
+                            <IconButton icon={<TrashIcon />} appearance="ghost" color="red" onClick={() => handleDeleteSocial(index)} />
+                        </div>
+                    ))}
+                </Form.Group>
+
+                <Form.Group>
+                    <ButtonToolbar>
+                        <Button appearance="primary" onClick={handleSubmit}>Submit</Button>
+                        <Button appearance="default">Cancel</Button>
+                    </ButtonToolbar>
+                </Form.Group>
+            </Form>
+        </div>
+    );
+};
+
+export default AddCompany;
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -5,6 +5,7 @@ import logo from "../../public/images/logo.jpg";
 import { getSession, signOut } from "next-auth/react";
 import { GetServerSideProps } from 'next';
 import { Avatar, Dropdown, Popover, Whisper, WhisperInstance } from 'rsuite';
+import AddCompany from './AddCompany';
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const session = await getSession(context);
     console.log(session)
@@ -26,7 +27,6 @@ interface ProtectedPageProps {
 }
 
 const Page = ({ session }: ProtectedPageProps) => {
-    console.log("session", session)
     const trigger = useRef<WhisperInstance>(null);
     useEffect(() => {
         import('bootstrap/dist/js/bootstrap.bundle.min.js');
@@ -62,24 +62,6 @@ const Page = ({ session }: ProtectedPageProps) => {
                     </div>
                     <div className='header-menu'>
                         <ul className='list-unstyled mb-0'>
-                            {/* <li>
-                                <form className="search-bar position-relative">
-                                    <input type="text" placeholder="Search here" />
-                                    <button className="search-btn2" type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
-                                </form>
-                            </li>
-                            <li className='position-relative nav-link'>
-                                <small className='d-inline-block'>3</small>
-                                <button className='nav-link'>
-                                    <i className='fa-regular fa-bell'></i>
-                                </button>
-                            </li>
-                            <li className='position-relative nav-link'>
-                                <small className='d-inline-block'>3</small>
-                                <button className='nav-link'>
-                                    <i className='fa-regular fa-comment'></i>
-                                </button>
-                            </li> */}
                             <li className='nav-item dropdown'>
                                 <Whisper placement="bottomEnd" trigger="click" ref={trigger} speaker={renderAdminSpeaker}>
                                     <Avatar
@@ -164,141 +146,11 @@ const Page = ({ session }: ProtectedPageProps) => {
                                     <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Profile</button>
                                 </li>
                                 <li className="nav-item" role="presentation">
-                                    <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Projects</button>
-                                </li>
-                                <li className="nav-item" role="presentation">
-                                    <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Bank & Statutory &nbsp; <span className="d-inline-block text-danger">(Admin Only)</span></button>
-                                </li>
-                                <li className="nav-item" role="presentation">
-                                    <button className="nav-link" id="pills-contact-tab2" data-bs-toggle="pill" data-bs-target="#pills-contact2" type="button" role="tab" aria-controls="pills-contact2" aria-selected="false">Assets</button>
+                                    <button className="nav-link" id="pills-company-tab" data-bs-toggle="pill" data-bs-target="#pills-company" type="button" role="tab" aria-controls="pills-company" aria-selected="false">Add Company</button>
                                 </li>
                             </ul>
                             <div className="tab-content" id="pills-tabContent">
                                 <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                                    <div className="profile-box generictab-box">
-                                        <h4>Personal Informations</h4>
-                                        <div className="personal-info">
-                                            <ul >
-                                                <li>
-                                                    <div className="title">Passport No.</div>
-                                                    <div className="text">9876543210</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Passport Exp Date.</div>
-                                                    <div className="text">9876543210</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Tel</div>
-                                                    <div className="text"><a href="javascript:;">9876543210</a></div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Nationality</div>
-                                                    <div className="text">Indian</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Religion</div>
-                                                    <div className="text">Christian</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Marital status</div>
-                                                    <div className="text">Married</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Employment of spouse</div>
-                                                    <div className="text">No</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">No. of children</div>
-                                                    <div className="text">2</div>
-                                                </li>
-                                            </ul>
-                                        </div>
-
-                                    </div>
-                                    <div className="profile-box generictab-box">
-                                        <h4>Emergency Contact</h4>
-                                        <h5 className="section-title">Primary</h5>
-                                        <div className="personal-info">
-                                            <ul>
-                                                <li>
-                                                    <div className="title">Name</div>
-                                                    <div className="text">John Doe</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Relationship</div>
-                                                    <div className="text">Father</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Phone</div>
-                                                    <div className="text">9876543210, 9876543210</div>
-                                                </li>
-                                            </ul>
-                                            <hr />
-                                            <h5 className="section-title">Secondary</h5>
-                                            <ul className="personal-info">
-                                                <li>
-                                                    <div className="title">Name</div>
-                                                    <div className="text">Karen Wills</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Relationship</div>
-                                                    <div className="text">Brother</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Phone</div>
-                                                    <div className="text">9876543210, 9876543210</div>
-                                                </li>
-                                            </ul>
-                                        </div>
-
-                                    </div>
-                                    <div className="profile-box generictab-box">
-                                        <h4>Bank information</h4>
-                                        <div className="personal-info">
-                                            <ul>
-                                                <li>
-                                                    <div className="title">Bank name</div>
-                                                    <div className="text">ICICI Bank</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">Bank account No.</div>
-                                                    <div className="text">159843014641</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">IFSC Code</div>
-                                                    <div className="text">ICI24504</div>
-                                                </li>
-                                                <li>
-                                                    <div className="title">PAN No</div>
-                                                    <div className="text">TC000Y56</div>
-                                                </li>
-                                            </ul>
-                                        </div>
-
-                                    </div>
-                                    <div className="profile-box generictab-box">
-                                        <h4>Family Informations</h4>
-                                        <div className="profile-main-table">
-                                            <table className="table profile-table mb-0">
-                                                <thead>
-                                                    <tr>
-                                                        <th>Name</th>
-                                                        <th>Relationship</th>
-                                                        <th>Date of Birth</th>
-                                                        <th>Phone</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td>Leo</td>
-                                                        <td>Brother</td>
-                                                        <td>Feb 16th,2023</td>
-                                                        <td>9876543210</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
                                     <div className="profile-box generictab-box">
                                         <h4>Education Informations </h4>
                                         <div className="profile-timeline">
@@ -338,14 +190,15 @@ const Page = ({ session }: ProtectedPageProps) => {
 
                                     </div>
                                 </div>
-                                <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-                                <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
-                                <div className="tab-pane fade" id="pills-contact2" role="tabpanel" aria-labelledby="pills-contact-tab2">...</div>
+                                <div className="tab-pane fade" id="pills-company" role="tabpanel" aria-labelledby="pills-company-tab">
+                                    <AddCompany />
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
         </>
     );
 };
