41fe596031580022025376d9fc7ccec66300f4e6
adnan425 <166127663+adnan425@users.noreply.github.com>
2024-11-23 22:35:45 +0400

    auth added

--- app/services/axiosInstance.ts
+++ app/services/axiosInstance.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { API_URL } from "../constants/constants";
+import { getSession } from "next-auth/react";
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
@@ -7,7 +8,11 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(async (config: any) => {
-
+    const session = await getSession();  // Retrieve the session from NextAuth.js 
+    if (session && session?.user?.token) {
+        config.headers['Authorization'] = `Bearer ${session?.user?.token}`;
+        config.headers['Content-Type'] = 'application/json';
+    }
 
     // config.headers['ngrok-skip-browser-warning'] = '69420';
 
